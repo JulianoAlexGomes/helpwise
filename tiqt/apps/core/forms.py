5f02f39407f303c862e6b61d2a846b8680d64a8b
JulianoAlexGomes <julianogomes@outlook.com.br>
2025-02-10 17:00:46 -0300

    updating_person

--- tiqt/apps/core/forms.py
+++ tiqt/apps/core/forms.py
@@ -2,6 +2,32 @@ from django import forms
 from django_select2.forms import ModelSelect2Widget
 from .models import Ticket, Cliente
 
+
+class ClienteForm(forms.ModelForm):
+    class Meta:
+        model = Cliente
+        fields = [
+                    'razao_social',
+                    'fantasia',
+                    'cnpj',
+                    'telefone',
+                    'email',
+                    'endereco',
+                    'numero',
+                    'bairro',
+                    'cep',
+                    'complemento',
+                    'cidade',
+                    'uf',
+                    'tributacao',
+                    'responsavel',
+                    'observacao',
+                    'ativo',
+                    'inativo',
+                ]
+
+
+    
 class TicketForm(forms.ModelForm):
     class Meta:
         model = Ticket
@@ -9,8 +35,3 @@ class TicketForm(forms.ModelForm):
         widgets = {
             'cliente': ModelSelect2Widget(model=Cliente, search_fields=['fantasia__icontains', 'razao_social__icontains']),
         }
-
-class ClienteForm(forms.ModelForm):
-    class Meta:
-        model = Cliente
-        fields = ['fantasia', 'razao_social', 'cnpj', 'endereco', 'telefone', 'email']

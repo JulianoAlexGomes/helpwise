8d4cc7c4f946c7139d4f54056108e16bd4922248
JulianoAlexGomes <julianogomes@outlook.com.br>
2025-02-17 11:11:00 -0300

    updating

--- tiqt/apps/core/tables.py
+++ tiqt/apps/core/tables.py
@@ -1,10 +1,20 @@
-from django_tables2 import Table, LinkColumn, A
-from tiqt.apps.core.models import Ticket
-
+from django_tables2 import Table, LinkColumn, A, tables
+from .models import Ticket
+from django.urls import reverse
 
 class TicketTable(Table):
     id = LinkColumn('ticket_detail', args=[A('pk')])
+    cliente = tables.Column(accessor='cliente.fantasia', verbose_name='Cliente')
+    ultimo_comentario = tables.Column(accessor='comentario_set.last.texto', verbose_name='Último Comentário')
 
     class Meta:
         model = Ticket
+        template_name = 'django_tables2/bootstrap.html'
+        fields = ('id', 'responsavel', 'status', 'cliente', 'ultimo_comentario')
         attrs = {'class': 'striped responsive-table'}
+
+    # def render(self):
+    #     return '<a href="{}">{}</a>'.format(
+    #         reverse('ticket_detail', args=[self.record.pk]),
+    #         super().render()
+    #     )

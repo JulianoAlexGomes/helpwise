6708524a9ec24adc78a5355131c052d58071de32
JulianoAlexGomes <julianogomes@outlook.com.br>
2025-09-04 14:05:06 -0300

    updating

--- tiqt/apps/core/admin.py
+++ tiqt/apps/core/admin.py
@@ -16,7 +16,8 @@ class ComentarioAdmin(admin.ModelAdmin):
     list_display = ['id', 'ticket', 'autor', 'criado_em']
     list_filter = ['ticket', 'autor']
 
-@admin.register(Comentario)
-class ComentarioAdmin(admin.ModelAdmin):
-    list_display = ['id', 'ticket', 'autor', 'criado_em']
-    list_filter = ['ticket', 'autor'] 
+# aqui preciso registrar o user para poder cadastrar novos usuarios
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff']
